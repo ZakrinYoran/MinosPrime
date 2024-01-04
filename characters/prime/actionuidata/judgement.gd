10055a9bae79d0810a4919f1842687cebdf9002a
ZakrinYoran <145646699+ZakrinYoran@users.noreply.github.com>
2024-01-03 20:55:09 -0900

    v0.4.1
    updated Judgement

--- characters/prime/actionuidata/judgement.gd
+++ characters/prime/actionuidata/judgement.gd
@@ -3,13 +3,13 @@ extends CharacterState
 #func _on_hit_something(obj, hitbox):
 #	iasa_at = 19
 
-func _frame_5():
+func _frame_7():
 	host.start_invulnerability()
 	host.colliding_with_opponent = false
 
-func _frame_7():
+func _frame_9():
 	var new_pos = host.opponent.get_pos()
-	var new_x = new_pos.x - 30*(host.get_facing_int())
+	var new_x = new_pos.x - 20*(host.get_facing_int())
 	host.set_pos(new_x, new_pos.y-10)
 	host.set_vel(host.opponent.get_vel().x, host.opponent.get_vel().y)
 	host.end_invulnerability()
